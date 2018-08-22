a177b198f9b3a0e534f976d000d75fd646e8717d
Dave Hummel <dmhummel@gmail.com>
2018-08-21 21:21:16 -0500

    no message

--- dh_logger/dh_logger.cpp
+++ dh_logger/dh_logger.cpp
@@ -3,56 +3,61 @@
 #include "Stream.h"
 #include <Arduino.h>
 
-
-
-void Logger::setStream(Stream* in){
+void Logger::setStream(Stream *in)
+{
 	stream = in;
 }
 
-
-bool Logger::print(uint8_t val) {
+bool Logger::print(uint8_t val)
+{
 	if (!validate(1))
 		return false;
-	memcpy(buffer+byteCount,&val,1);
+	memcpy(buffer + byteCount, &val, 1);
 	byteCount++;
 	return true;
 }
-bool Logger::print(int8_t val) {
+bool Logger::print(int8_t val)
+{
 	if (!validate(1))
 		return false;
-		memcpy(buffer+byteCount,&val,1);
-		byteCount++;
+	memcpy(buffer + byteCount, &val, 1);
+	byteCount++;
 	return true;
 }
-bool Logger::print(uint16_t val) {
+bool Logger::print(uint16_t val)
+{
 	if (!validate(2))
 		return false;
-		memcpy(buffer+byteCount,&val,2);
-		byteCount+=2;
+	memcpy(buffer + byteCount, &val, 2);
+	byteCount += 2;
 	return true;
 }
-bool Logger::print(int16_t val) {
+bool Logger::print(int16_t val)
+{
 	if (!validate(2))
 		return false;
-		memcpy(buffer+byteCount,&val,2);
-		byteCount+=2;
+	memcpy(buffer + byteCount, &val, 2);
+	byteCount += 2;
 	return true;
 }
-bool Logger::print(uint32_t val) {
+bool Logger::print(uint32_t val)
+{
 	if (!validate(4))
 		return false;
-		memcpy(buffer+byteCount,&val,4);
-		byteCount+=4;
+	memcpy(buffer + byteCount, &val, 4);
+	byteCount += 4;
 	return true;
 }
-bool Logger::print(int32_t val) {
+bool Logger::print(int32_t val)
+{
 	if (!validate(4))
 		return false;
-		memcpy(buffer+byteCount,&val,4);
-		byteCount+=4;
-		return true;
+	memcpy(buffer + byteCount, &val, 4);
+	byteCount += 4;
+	return true;
 }
-bool Logger::print(bool val) {
+bool Logger::print(bool val)
+{
 	if (!validate(1))
 		return false;
 	buffer[byteCount] = val;
@@ -60,45 +65,51 @@ bool Logger::print(bool val) {
 	return true;
 }
 
-bool Logger::print(float val) {
+bool Logger::print(float val)
+{
 	if (!validate(4))
 		return false;
-	memcpy(buffer+byteCount,&val,4);
-	byteCount+=4;
+	memcpy(buffer + byteCount, &val, 4);
+	byteCount += 4;
 	return true;
 }
-bool Logger::print(double val) {
+bool Logger::print(double val)
+{
 	if (!validate(8))
 		return false;
-	memcpy(buffer+byteCount,&val,8);
-	byteCount+=8;
+	memcpy(buffer + byteCount, &val, 8);
+	byteCount += 8;
 	return true;
 }
-bool Logger::print(const char text[], uint8_t len) {
-	if (len == 0){
-		for (; len <1024; len++){
-			if (text[len]=='\0')
+bool Logger::print(const char text[], uint8_t len)
+{
+	if (len == 0)
+	{
+		for (; len < 1024; len++)
+		{
+			if (text[len] == '\0')
 				break;
 		}
 	}
 	if (!validate(len))
 		return false;
-	memcpy(buffer+byteCount,text,len);
-	byteCount+=len;
-  return true;
+	memcpy(buffer + byteCount, text, len);
+	byteCount += len;
+	return true;
 }
 
-
-bool Logger::batchSend() {
+bool Logger::batchSend()
+{
 	if (!inBatchSend)
 		return false;
-	setHeader(byteCount-4);
+	setHeader(byteCount - 4);
 	flushBuffer();
 	inBatchSend = false;
 	return true;
 }
 
-void Logger::sendTimeSync(uint32_t time) {
+void Logger::sendTimeSync(uint32_t time)
+{
 	if (inStreamSend || inBatchSend)
 		return;
 
@@ -108,7 +119,7 @@ void Logger::sendTimeSync(uint32_t time) {
 
 	tmp[1] = 0;
 
-  tmp[2] = 0xFF;
+	tmp[2] = 0xFF;
 
 	tmp[3] = 0xFF;
 
@@ -123,7 +134,8 @@ void Logger::sendTimeSync(uint32_t time) {
 	stream->write(tmp, 8);
 }
 
-void Logger::sendLineSync() {
+void Logger::sendLineSync()
+{
 	if (inStreamSend || inBatchSend)
 		return;
 
@@ -133,7 +145,7 @@ void Logger::sendLineSync() {
 
 	tmp[1] = 0;
 
-  tmp[2] = 0xFF;
+	tmp[2] = 0xFF;
 
 	tmp[3] = 0xFF;
 
@@ -141,7 +153,8 @@ void Logger::sendLineSync() {
 	//Serial.println("poof");
 }
 
-bool Logger::startBatchSend( char mod, uint32_t instID){
+bool Logger::startBatchSend(char mod, uint32_t instID)
+{
 	if (inStreamSend || inBatchSend)
 		return false;
 
@@ -152,23 +165,26 @@ bool Logger::startBatchSend( char mod, uint32_t instID){
 	return true;
 }
 
-bool Logger::startStreamSend(uint16_t sendSize, char mod, uint32_t instID) {
+bool Logger::startStreamSend(uint16_t sendSize, char mod, uint32_t instID)
+{
 	if (inStreamSend || inBatchSend)
 		return false;
 	module = mod;
 	id = instID;
 
 	byteCount = HEADER_SIZE;
-	setHeader(sendSize+HEADER_SIZE-4);
+	setHeader(sendSize + HEADER_SIZE - 4);
 	inStreamSend = true;
 	streamRemainder = sendSize;
 	return true;
 }
 
-uint16_t Logger::streamSend() {
+uint16_t Logger::streamSend()
+{
 	if (!inStreamSend)
 		return 9999;
-	if (streamRemainder > 0){
+	if (streamRemainder > 0)
+	{
 		return streamRemainder;
 	}
 	flushBuffer();
@@ -176,19 +192,24 @@ uint16_t Logger::streamSend() {
 	return 0;
 }
 
-bool Logger::validate(uint8_t size) {
+bool Logger::validate(uint8_t size)
+{
 	if (!inStreamSend && !inBatchSend)
 		return false;
 
-	if (LOG_BUFF - byteCount < size) {
-		if (inStreamSend) {
+	if (LOG_BUFF - byteCount < size)
+	{
+		if (inStreamSend)
+		{
 			flushBuffer();
 		}
-		else {
+		else
+		{
 			return false;
 		}
 	}
-	if (inStreamSend) {
+	if (inStreamSend)
+	{
 		if (streamRemainder < size)
 			return false;
 		else
@@ -197,30 +218,36 @@ bool Logger::validate(uint8_t size) {
 	return true;
 }
 
-void Logger::setHeader(uint16_t length) {
+void Logger::setHeader(uint16_t length)
+{
 	buffer[0] = (byte)(length & 0xff);
 	buffer[1] = (byte)((length >> 8) & 0xff);
 	buffer[2] = 0xff;
 	buffer[3] = 0xff;
- 	buffer[4] = (byte)module;
+	buffer[4] = (byte)module;
 	buffer[5] = (byte)(id & 0xff);
 	buffer[6] = (byte)((id >> 8) & 0xff);
 	buffer[7] = (byte)((id >> 16) & 0xff);
 	buffer[8] = (byte)((id >> 24) & 0xff);
 }
-void Logger::flushBuffer() {
+void Logger::flushBuffer()
+{
 	stream->write(buffer, byteCount);
 	byteCount = 0;
 }
 
-void Logger::abortSend(){
-	if (inBatchSend){
+void Logger::abortSend()
+{
+	if (inBatchSend)
+	{
 		inBatchSend = false;
 		return;
 	}
-	if (inStreamSend){
+	if (inStreamSend)
+	{
 		flushBuffer();
-		for (uint16_t i = 0; i < streamRemainder; i++){
+		for (uint16_t i = 0; i < streamRemainder; i++)
+		{
 			stream->write((byte)0);
 		}
 		inStreamSend = false;
@@ -228,14 +255,15 @@ void Logger::abortSend(){
 	}
 }
 
-void ErrorLogger::clearError(){
+void ErrorLogger::clearError()
+{
 	errBufCount = 0;
 	errorCode = NONE;
 	errorTime = 0;
 	errorComplete = false;
 
- charLoc = 0;
- hasParseError = false;
+	charLoc = 0;
+	hasParseError = false;
 	errBufCount = 0;
 	errorBuffer[0] = '\0';
 	errorCode = NONE;
@@ -243,37 +271,45 @@ void ErrorLogger::clearError(){
 	errorComplete = false;
 }
 
-void ErrorLogger::setParseError(char input[],uint16_t charPos,const char* message){
-	  if (!hasParseError){
-			println(input);
-			for (uint16_t i = 0; i < charPos; i++)
-				print(' ');
-			println('^');
-			hasParseError = true;
-		}
-		print(message);
-		print(" @ ");
-		println(charPos);
+void ErrorLogger::setParseError(char input[], uint16_t charPos, const char *message)
+{
+	if (!hasParseError)
+	{
+		println(input);
+		for (uint16_t i = 0; i < charPos; i++)
+			print(' ');
+		println('^');
+		hasParseError = true;
+	}
+	print(message);
+	print(" @ ");
+	println(charPos);
 }
 
-char* ErrorLogger::getErrorText(){
+char *ErrorLogger::getErrorText()
+{
 	errorBuffer[errBufCount] = '\0';
 	return errorBuffer;
 }
 
-uint32_t ErrorLogger::getErrorTime(){
+uint32_t ErrorLogger::getErrorTime()
+{
 	return errorTime;
 }
 
-ErrorLogger::ERROR_CODE ErrorLogger::getErrorCode(){
+ErrorLogger::ERROR_CODE ErrorLogger::getErrorCode()
+{
 	return errorCode;
 }
 
-size_t ErrorLogger::write(uint8_t ch){
-	if (errorComplete){
+size_t ErrorLogger::write(uint8_t ch)
+{
+	if (errorComplete)
+	{
 		clearError();
 	}
-	if (errBufCount == ERR_BUFF-1){
+	if (errBufCount == ERR_BUFF - 1)
+	{
 		return 0;
 	}
 	errorBuffer[errBufCount] = ch;
@@ -281,11 +317,13 @@ size_t ErrorLogger::write(uint8_t ch){
 	return 1;
 }
 
-void ErrorLogger::finished(uint32_t time, ERROR_CODE code){
+void ErrorLogger::finished(uint32_t time, ERROR_CODE code)
+{
 	errorComplete = true;
 	errorTime = time;
 	errorCode = code;
-	if (stream!=0){
-		stream->println(errorBuffer);
+	if (stream != 0)
+	{
+		stream->println(getErrorText());
 	}
 }
